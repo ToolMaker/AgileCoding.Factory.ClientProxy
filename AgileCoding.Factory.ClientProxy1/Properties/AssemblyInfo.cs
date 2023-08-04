bbcfaa3899468309d0c07d4b960367ac4bdc180f
Ernie Gunning <gunning.ernie@gmail.com>
2023-08-04 11:06:34 +0200

    Moving to core

--- AgileCoding.Factory.ClientProxy1/Properties/AssemblyInfo.cs
+++ AgileCoding.Factory.ClientProxy1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AgileCoding.Factory.ClientProxy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Factories relating to service proxies")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AgileCoding.Factory.ClientProxy")]
-[assembly: AssemblyCopyright("Ernie Gunning")]
+[assembly: AssemblyCopyright("Copyright © Ernie Gunning  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.25")]
-[assembly: AssemblyFileVersion("1.0.0.25")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
